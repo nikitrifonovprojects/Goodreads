5c111eb8e60050d00fe5cc3f636b1e46f9b05f78
nikitrifonovprojects <do_not_stop@abv.bg>
2018-05-21 18:05:16 +0300

    #0 - added Data project and DB context

---
+++ Goodreads/Goodreads.Data/GoordeadsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Goodreads.Data
+{
+    public class GoordeadsContext : DbContext
+    {
+        public GoordeadsContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+    }
+}
--- Goodreads/Goodreads/Startup.cs
+++ Goodreads/Goodreads/Startup.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Goodreads.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,7 +11,7 @@ namespace Goodreads
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +20,8 @@ namespace Goodreads
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<GoordeadsContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("GoodreadsConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
