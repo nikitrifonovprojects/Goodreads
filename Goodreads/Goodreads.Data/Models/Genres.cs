3f9a188f9fc2b1b55858a9b206819758928fedf0
nikitrifonovprojects <do_not_stop@abv.bg>
2018-05-23 16:17:57 +0300

    #00

---
+++ Goodreads/Goodreads.Data/Models/Genres.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Goodreads.Data.Models
+{
+    public class Genres
+    {
+        [Range(1, -int.MaxValue)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
